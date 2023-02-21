666cbbe1c980c5fd4c6f14a221c26c3f409312ed
mrsimonsen <johnsimonsen@weber.edu>
2023-02-21 14:58:26 -0700

    Passign References to Alter Arugments

---
+++ Chapter 6/referencing.cpp
@@ -0,0 +1,26 @@
+// Referencing
+// Demonstrates using references
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+	int myScore = 1000;
+	int& mikesScore = myScore; //create a reference
+
+	cout << "myScore is: " << myScore << endl;
+	cout << "mikesScore is: " << mikesScore << "\n\n";
+
+	cout <<"Adding 500 to myScore\n";
+	myScore += 500;
+	cout << "myScore is: " << myScore << endl;
+	cout << "mikesScore is: " << mikesScore << "\n\n";
+
+	cout << "Adding 500 to mikesScore\n";
+	mikesScore += 500;
+	cout << "myScore is: " << myScore << endl;
+	cout << "mikesScore is: " << mikesScore << "\n\n";
+
+	return 0;
+}
