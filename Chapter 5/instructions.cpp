3b4cf0650e1f354933516cdd45f3c7818c28064f
mrsimonsen <johnsimonsen@weber.edu>
2023-02-20 20:37:41 -0700

    Using Defult Arguments, 162

---
+++ Chapter 5/instructions.cpp
@@ -0,0 +1,20 @@
+// Instructions
+// Demonstrates writting new functions
+
+#include <iostream>
+
+using namespace std;
+
+//function protoype (declaration)
+void instructions();
+
+int main(){
+	instructions();
+	return 0;
+}
+
+//function definition
+void instructions(){
+	cout << "Welcome to the most fun you've even had with text!\n\n";
+	cout << "Here's how to play the game ...\n";
+}
